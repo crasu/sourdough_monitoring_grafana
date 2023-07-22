636d31bf9d3647b0828f7e778e43d926a2592746
Christian Krämer <spam@crasu.de>
2023-07-22 15:16:10 +0200

    remove graphana code

--- src/config.h
+++ src/config.h
@@ -4,17 +4,9 @@
     #include "config_secret.h"
 #endif
 
-/*
-Add and set these values in config_secret.h
-
-#define WIFI_SSID     "ssid" // Add wifi name
-#define WIFI_PASSWORD "key" // Add wifi passowrd
-#define GC_PROM_USER "username"
-#define GC_PROM_PASS "cloud secret"
-*/
-
 #define ID "sourdough" // Add unique name for this sensor
-#define INTERVAL 5 //seconds
+#define INTERVAL 15 //seconds
+#define MEASUREMENTS_PER_HOUR 20 //normally 4
 
 #define DHTPIN 32    // Which pin is DHT 11 connected to
 #define DHTTYPE DHT11 // Type DHT 11
@@ -22,6 +14,4 @@ Add and set these values in config_secret.h
 #define ULTRASONIC_PIN_TRIG 4 // Which pin is HC-SR04's trig connected txo
 #define ULTRASONIC_PIN_ECHO 5 // Which pin is HC-SR04's echo connected to
 
-#define GC_PORT 443
-#define GC_PROM_URL "prometheus-prod-13-prod-us-east-0.grafana.net"
-#define GC_PROM_PATH "/api/prom/push"
+#define EMULATE_SENSOR 1
